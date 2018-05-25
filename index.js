2cdf1c900e7a362d128b928108acde3056edc8c3
Samparkyn <samparkyn101@gmail.com>
2018-05-25 18:53:54 +0200

    Refactor & add functionality to chain operations

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ let operators
 let clearButton
 let equalsButton
 let decimalButton
+let displayScreen
+let operationScreen
 let displayNum = ''
-let currentNum = ''
-let oldNum = ''
+let lastNumClicked = ''
+let storedNum = ''
 let storedOperator = ''
 let resultNum
-let storedEntriesArray = []
+let storedEntries = []
 
 const operatorFunctions = {
   '+': function(a, b) { return parseFloat(a) + parseFloat(b) }, // add
@@ -25,6 +27,8 @@ function startApp() {
   clearButton = document.getElementById('clear')
   equalsButton = document.getElementById('equals')
   decimalButton = document.getElementById('decimal')
+  displayScreen = document.getElementById('display')
+  operationScreen = document.getElementById('operations')
   addListeners()
 }
 
@@ -40,77 +44,97 @@ function addListeners() {
 
 function handleNumberClick (e) {
   const numClicked = e.target.value
-  currentNum += numClicked
-  displayNum += numClicked
-  storedEntriesArray.push(numClicked)
-  console.log('currentNum inside number click', currentNum)
-  displayValue()
+
+  if (resultNum) {
+    storedNum = resultNum
+    lastNumClicked += numClicked
+    displayNum += numClicked
+    displayScreen.value = displayNum
+    storedEntries.push(numClicked)
+    operationScreen.value = storedEntries.join(' ')
+  } else {
+    lastNumClicked += numClicked
+    displayNum += numClicked
+    displayScreen.value = displayNum
+    storedEntries.push(numClicked)
+    operationScreen.value = storedEntries.join(' ')
+    console.log('lastNumClicked inside number click', lastNumClicked)
+    console.log('displayNum inside number click', displayNum)
+    // displayValue()
+  }
 }
 
 
 function handleOperatorClick(e) {
   const operator = e.target.value
-  storedOperator = operator
-  
-  if (resultNum) {
-    oldNum = resultNum
-  } else {
-    oldNum = currentNum
-  }
 
-  storedEntriesArray.push(operator)
-  console.log('old num indside operater click', oldNum)
-  currentNum = ''
-  displayNum = ''
-  displayOperator()
+  if (storedOperator) {
+    //if an op has already been pressed, calculate the 2 stored nums
+    calculate()
+    storedOperator = operator
+    storedEntries.push(operator)
+    operationScreen.value = storedEntries.join(' ')
+    displayNum = ''
+  } else {
+    storedOperator = operator
+    storedNum = lastNumClicked 
+    lastNumClicked = ''
+    displayNum = ''
+    storedEntries.push(operator)
+    operationScreen.value = storedEntries.join(' ')
+    console.log(storedOperator)
+    displayOperator()
+  }
 }
 
 
 function handleDecimalClick(e) {
-  let displayScreen = document.getElementById('display')
   displayScreen.value = e.target.value
 }
 
 
-function displayValue() {
-  let displayScreen = document.getElementById('display')
-  displayScreen.value = displayNum
-}
+// function displayValue() {
+//   let displayScreen = document.getElementById('display')
+//   displayScreen.value = displayNum
+// }
 
 function displayOperator() {
-  let displayScreen = document.getElementById('display')
   displayScreen.value = storedOperator
   console.log('op', storedOperator)
 }
 
+function resetValues() {
+  displayNum = ''
+  lastNumClicked = ''
+  storedNum = ''
+  resultNum = ''
+  storedOperator = ''
+  storedEntries = []
+}
+
 
 function clearScreen() {
-  let displayScreen = document.getElementById('display')
-  displayNum = ''
-  currentNum = ''
-  storedOperator = ''
-  storedEntriesArray = []
+  resetValues()
   displayScreen.value = displayNum
+  operationScreen.value = storedEntries
   console.log(displayNum)
 }
 
 function calculate() {
-  let displayScreen = document.getElementById('display')
-  if (currentNum && oldNum) {
-    oldNum = parseFloat(oldNum);
-    currentNum = parseFloat(currentNum);
-    resultNum = operatorFunctions[storedOperator](oldNum, currentNum)
-    displayScreen.value = resultNum
-    oldNum = resultNum
+  if (storedNum && lastNumClicked) {
+    resultNum = operatorFunctions[storedOperator](storedNum, lastNumClicked)
+    console.log('storedNum & lastNumClicked', storedNum, storedOperator, lastNumClicked)
+    storedNum = resultNum
+    lastNumClicked = ''
+    displayScreen.value = resultNum 
+
     console.log('= was pressed')
-    console.log('oldNum & currentNum', oldNum, storedOperator, currentNum)
     console.log('displayNum', displayNum)
     console.log('resultNum', resultNum)
-    console.log('stored entries arry', storedEntriesArray)
-    return resultNum
+    // console.log('stored entries arry', storedEntriesArray)
+
   } else {
-    displayNum = currentNum
-    resultNum = currentNum
+    displayScreen.value = lastNumClicked
   }
 
   
